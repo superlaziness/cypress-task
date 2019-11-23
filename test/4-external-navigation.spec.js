24b60c6cac8495a7ededf73849901df92410f073
denidoman@gmail.com <Born91>
2019-11-23 20:07:13 +0300

    not implemented test cases marked as skipped

--- test/4-external-navigation.spec.js
+++ test/4-external-navigation.spec.js
@@ -3,11 +3,15 @@ describe('4 External navigation', () => {
         cy.visit('https://www.jetbrains.com/help/idea/installation-guide.html');
     });
 
-    it('1 Next article link should lead to next page regardless of menu hierarchy', () => {});
+    // @Manual
+    it.skip('1 Next article link should lead to next page regardless of menu hierarchy', () => {});
 
-    it('2 Previous article link should lead to previous page regardless of menu hierarchy', () => {});
+    // @Manual
+    it.skip('2 Previous article link should lead to previous page regardless of menu hierarchy', () => {});
 
-    it('3 Browser Previous button should navigate to previous page', () => {});
+    // @Manual
+    it.skip('3 Browser Previous button should navigate to previous page', () => {});
 
-    it('4 Browser Next button should navigate to next page', () => {});
+    // @Manual
+    it.skip('4 Browser Next button should navigate to next page', () => {});
 });
--- test/5-keyboard-navigation.spec.js
+++ test/5-keyboard-navigation.spec.js
@@ -3,9 +3,12 @@ describe('5 Keyboard navigation', () => {
         cy.visit('https://www.jetbrains.com/help/idea/installation-guide.html');
     });
 
-    it('1 Tab keyboard button should select next node in menu', () => {});
+    // @Manual
+    it.skip('1 Tab keyboard button should select next node in menu', () => {});
 
-    it('2 Enter keyboard button should select node and open content', () => {});
+    // @Manual
+    it.skip('2 Enter keyboard button should select node and open content', () => {});
 
-    xit('3 Menu should be scrolled when bottom node in visible area is selected', () => {});
+    // @Manual
+    it.skip('3 Menu should be scrolled when bottom node in visible area is selected', () => {});
 });
